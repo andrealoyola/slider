4df59ed1c1257938e2b4724e535713dee5d678af
ANDREA CRISTINA LOYOLA DELGADO <andrea.loyolad@ucuenca.edu.ec>
2025-01-23 12:22:38 -0500

    actualizacion final

--- sketch.js
+++ sketch.js
@@ -1,19 +1,26 @@
 var sliders = [];
-
 var angulo = 0;
 
 function setup() {
   noCanvas();
-  //createCanvas (200, 200);
-  for (var i = 0; i < 100; i++) {
-    sliders[i] = createSlider(0, 255, 50);
-  }
 
-  sliders[0].input(ajustarSliders);
+  for (var i = 0; i < 250; i++) {
+    sliders[i] = createSlider(0, 255, 50);
+    sliders[i].style("width", "200px"); // Ajusta el tamaño del slider
+  }
 }
 
-function ajustarSliders() {
-  for (var i = 1; i < sliders.length; i++) {
-    sliders[i].value(sliders[0].value());
+function draw() {
+  var offset = 0;
+  for (var i = 0; i < sliders.length; i++) {
+    var x = map(sin(angulo + offset), -1, 1, 0, 255);
+    sliders[i].value(x);
+
+    // Cambia el color de fondo del slider según su valor
+    var colorValue = `rgb(${x}, ${255 - x}, 150)`; // Genera un color dinámico
+    sliders[i].style("background", colorValue);
+
+    offset += 0.01;
   }
+  angulo += 0.1;
 }
